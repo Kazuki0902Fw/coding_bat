27dc9501ad6f9b17676659b0c397213e08fcc7ab
Kazuki0902Fw <f-96oks@akane.waseda.jp>
2017-12-25 12:21:17 +0900

    warmup_2

--- warmup-1,2.py
+++ warmup-1,2.py
@@ -61,3 +61,66 @@ def front_back(str):
 # front3
 def front3(str):
   return str[:3]*3
+
+
+"""warmup_2"""
+# string_times
+def string_times(str, n):
+  return str*n
+
+ # front_times
+ def front_times(str, n):
+  return str[:3]*n
+
+# string_bits
+def string_bits(str):
+  return  str[::2]
+
+# string_splosion
+def string_splosion(str):
+  pre = str[0]
+  new_str = [pre]
+  for i in str[1:]:
+    pre += i
+    new_str.append(pre)
+  return ''.join(new_str)
+
+def string_splosion(str):
+  result = ""
+  for i in range(len(str)):
+    result = result + str[:i+1]
+  return result
+
+# last2
+def last2(str):
+  count=0
+  for i in range(len(str)-2):
+  if str[-2:] == str[i:i+2]:
+    count+=1
+  return count
+
+# array_count9
+def array_count9(nums):
+  return nums.count(9)
+
+# array_front9
+def array_front9(nums):
+  return 9 in nums[:3]
+
+# array123
+def array123(nums):
+  return '123' in ''.join([str(i) for i in nums])
+
+def array123(nums):
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False
+
+# string_match
+def string_match(a, b):
+  count=0
+  for i in range(0, min(len(a), len(b))-1):
+    if a[i:i+2] == b[i:i+2]:
+      count+=1
+  return count
